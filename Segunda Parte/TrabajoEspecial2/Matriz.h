54a07ba595a87d479ea4f0be0660986242182f4d
nicolasattilio <nicolasattilio@gmail.com>
2017-06-26 18:02:45 -0300

    Añadiendo segunda parte

---
+++ Segunda Parte/TrabajoEspecial2/Matriz.h
@@ -0,0 +1,19 @@
+#ifndef MATRIZ_H_INCLUDED
+#define MATRIZ_H_INCLUDED
+
+template <typename generico>
+class Matriz{
+public:
+    Matriz();
+    ~Matriz();
+    void suma
+    void resta
+    void multi
+    void subMatriz
+
+private:
+    struct T ** Matriz;
+
+};
+
+#endif // MATRIZ_H_INCLUDED
